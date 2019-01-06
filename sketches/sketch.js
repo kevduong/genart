741ceacf13390952ec1a4d4a7066fcd2daa384f7
kevduong <duong.kev@gmail.com>
2019-01-05 23:02:17 -0800

    randomness

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,4 +1,6 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 // Defines our parameter for our artwork
 const settings = {
@@ -12,25 +14,37 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 40;
 
     // Creating two dimensional grid
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
-        const u = x / count;
-        const v = y / count;
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
         points.push([ u, v ]);
       }
     }
     return points;
   };
 
-  const points = createGrid();
-  console.log(points);
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
   
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    points.forEach(([ u, v ]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 5, 0, Math.PI * 2, false);
+      context.strokeStyle = 'black';
+      context.lineWidth = 10;
+      context.stroke();
+    });
   };
 };
 
