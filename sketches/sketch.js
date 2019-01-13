7a1c16ff9410cb622a775c6e8ea108dea2f5c9e5
unknown <duong.kev@gmail.com>
2019-01-13 13:26:53 -0800

    added rotation and fillText

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -28,7 +28,8 @@ const sketch = () => {
         const v = count <= 1 ? 0.5 : y / (count - 1);
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          radius: Math.abs(0.01 + random.gaussian() * 0.1),
+          rotation: random.noise2D(u, v),
           position: [ u, v ]
         });
       }
@@ -36,7 +37,7 @@ const sketch = () => {
     return points;
   };
 
-  random.setSeed(512);
+  // random.setSeed(512);
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
   
@@ -48,7 +49,8 @@ const sketch = () => {
       const {
         position,
         radius,
-        color
+        color,
+        rotation
       } = data;
 
       const [ u, v] = position;
@@ -56,11 +58,21 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-      context.strokeStyle = 'black';
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.strokeStyle = 'black';
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('=', 0, 0);
+
+      context.restore();
+
     });
   };
 };
